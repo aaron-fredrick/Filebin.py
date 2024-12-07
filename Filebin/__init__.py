fe14f904883767c298c0e75231d2632b9a040f06
Todo Lodo <me@todolodo.xyz>
2024-12-08 02:11:03 +0530

    docstrings

--- Filebin/__init__.py
+++ Filebin/__init__.py
@@ -16,11 +16,28 @@ class API:
         self.__bins = {}
 
     @property
-    def bins(self) -> dict[_BIN]:
+    def bins(self) -> dict[str, _BIN]:
         return self.__bins
 
     # API public methods
     async def getBin(self, _id: str, _from_cache: bool = False) -> Optional[_BIN]:
+        """
+        Retrieve a bin from Filebin by its unique identifier.
+
+        Args:
+            _id (str): The unique identifier of the bin to retrieve.
+            _from_cache (bool, optional): If True, fetch the bin from the local cache
+                instead of making an API call. Defaults to False.
+
+        Returns:
+            Optional[_BIN]: An instance of the `_BIN` class if the bin is found,
+            either from the cache or the API. Returns None if the bin is not found
+            and `_from_cache` is True.
+
+        Raises:
+            InvalidBin: If the bin ID is not found on the server.
+        """
+
         if _from_cache:
             return self.bins.get(_id, None)
         else:
@@ -38,6 +55,25 @@ class API:
             return self.__bins[_id]
 
     async def getFile(self, _bin_id: str, _file_name: str, _from_cache: bool = False) -> Optional[_FILE]:
+        """
+        Retrieve a file from a specific bin in Filebin.
+
+        Args:
+            _bin_id (str): The unique identifier of the bin containing the file.
+            _file_name (str): The name of the file to retrieve.
+            _from_cache (bool, optional): If True, fetch the file from the local
+                cache instead of making an API call. Defaults to False.
+
+        Returns:
+            Optional[_FILE]: An instance of the `_FILE` class if the file is found,
+            either from the cache or the API. Returns None if the bin or file is not
+            found and `_from_cache` is True.
+
+        Raises:
+            InvalidBin: If the bin ID is not found on the server.
+            InvalidFile: If the file name is not found in the bin.
+        """
+
         # Fetch bin from cache if available
         bin = self.bins.get(_bin_id)
 
